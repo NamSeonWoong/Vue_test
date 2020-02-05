31fd520988083773fec8ff3e1d79079c0e539802
남선웅 <wun1107@gmail.com>
2020-02-05 19:16:11 +0900

    vue_test

---
+++ todolist-Basic/src/components/ListAdd.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <v-textarea
+          outlined
+          v-model="memo"
+          label="투두리스트를 입력하시오"
+          value=""
+        ></v-textarea>
+        <v-btn @click="listAdd">리스트 추가</v-btn>
+    </div>
+</template>>
+
+<script>
+export default {
+    data()  {
+        return {
+            memo: null
+        }
+    },
+    methods:    {
+        listAdd()   {
+            console.log("리스트 추가")
+        }
+    }
+}
+</script>>
