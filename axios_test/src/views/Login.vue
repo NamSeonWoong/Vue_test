f411cc874a7b4374fd8dc803ab1ea26c91c5e35a
남선웅 <wun1107@gmail.com>
2020-02-06 00:29:03 +0900

    axios get통신

---
+++ axios_test/src/views/Login.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-btn @click="test">테스트</v-btn>
+</template>
+
+<script>
+
+import axios from "axios"
+
+export default {
+    methods:    {
+        test()  {
+            
+// Make a request for a user with a given ID
+axios
+    .get('https://reqres.in/api')
+  .then(res => {
+    // handle success
+    console.log(res);
+  })
+  .catch(err => {
+    // handle error
+    console.log(err);
+  })
+  .then(()  => {
+    // always executed
+    console.log("test")
+  });
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
