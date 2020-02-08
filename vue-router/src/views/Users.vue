0ae7497b8d7b197bd3eb6466de91123fe1eb54ea
남선웅 <wun1107@gmail.com>
2020-02-08 23:41:45 +0900

    router기본

---
+++ vue-router/src/views/Users.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      <h1>Users</h1>
+      <p>이 유저의 현재 유저번호가{{UserId}}입니다.</p>
+      <h1>그룹: {{$route.query.group}}</h1>
+      <h1>카테고리: {{$route.query.category}}</h1>
+      <h1>이름: {{$route.query.name}}</h1>
+      <h1>주소: {{$route.query.address}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    computed:   {
+        UserId()    {
+            return this.$route.params.userId
+        }
+    },
+    created() {
+        console.log("router",this.$router)
+        console.log("route",this.$route)
+    }
+}
+</script>
+
+<style>
+
+</style>
